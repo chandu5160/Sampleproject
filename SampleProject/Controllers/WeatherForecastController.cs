a969f519acdca718962b08977efb3f108fb071f6
chandrasekhara <chandrasekhara@proarch.com>
2022-10-06 16:45:55 +0530

    test

--- SampleProject/Controllers/WeatherForecastController.cs
+++ SampleProject/Controllers/WeatherForecastController.cs
@@ -26,6 +26,7 @@ namespace SampleProject.Controllers
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
+            //this is test
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
